4555fbb0fc5354123e0001aa3206e507ee06e939
Ji Bo <jibo.tiger@gmail.com>
2013-09-19 13:23:25 +0800

    add auto_configure.sh to port SDL to embedded environment Signed-off-by: Ji Bo <jibo.tiger@gmail.com>

---
+++ auto_configure.sh
@@ -0,0 +1,6 @@
+./configure  --host=arm-none-linux-gnueabi \
+--prefix=$HOME/work/embedded_lib/SDL      \
+--enable-audio --enable-video  --enable-events --disable-video-fbcon \
+--disable-video-directfb --disable-video-opengl \
+--enable-video-x11 --disable-dga \
+--disable-video-dga  --enable-oss --disable-alsa \
